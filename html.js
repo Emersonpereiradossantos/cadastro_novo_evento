75980f22c893b00c2583e602af4d9da202065b19
Emerson <96029393+Emersonpereiradossantos@users.noreply.github.com>
2022-10-24 10:03:52 -0300

    cadastro_novo_evento

---
+++ html.js
@@ -0,0 +1,34 @@
+let quantidadedepalestrantes = 6
+
+    if (quantidadedepalestrantes < 10) {
+        console.log("Quantidade permitida")
+    }else {
+        console.log("Não cadastrar")
+    }
+
+    let quantidadedeparticipantes = 94
+
+    if (quantidadedeparticipantes <= 100) {
+        console.log("Acesso liberado")
+    }else {
+        console.log("Acima do limite permitido")
+    }
+
+
+    let diaparaseinscrever = 15
+
+    if (diaparaseinscrever > 13) {
+        console.log("incrição permitida ")
+    }else {      
+        console.log("Prazo esgotado ")
+    }
+
+    let idade = 100
+
+    if (idade >= 18) {
+        console.log("Acesso liderado")
+    }else {
+        console.log("Idade insuficiente, não permitir a entrada")
+    }
+
+ 
